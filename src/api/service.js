28c7b65d3433a455dbd6d145ae8c652b48b4cc17
MarcosDetalis <detalismarcos@gmail.com>
2023-02-10 21:53:08 -0300

    ser

--- src/api/service.js
+++ src/api/service.js
@@ -1,22 +1,5 @@
 import axios from "axios";
- export async function getPersojes() {
-   const res = await fetch("https://swapi.dev/api/people/");
-   const data = await res.json();
-   return data;
- }
-
- export async function getSelecPersonaje(id) {
-   const res = await fetch("https://swapi.dev/api/" + id + "/");
-   const data = await res.json();
-   return data;
- }
-
- export async function getBusqueda(name) {
-   const res = await fetch("https://swapi.dev/api/people/?search=" + name);
-   const data = await res.json();
-   return data;
- }
-
+ 
 
 export async function getAxios(ite,id) {
   
